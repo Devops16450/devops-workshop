6200bf97f3398fcc91f1a90d07e6576aef9c037f
tsri <tsri010203@gmail.com>
2024-03-04 16:50:08 +0530

    file

--- terraform_code/V1_EC2.tf
+++ terraform_code/V1_EC2.tf
@@ -1,7 +1,6 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = "AKIAX236W3YQ4H47LNWO"
-    secret_key = "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"
+    
 }
 
 resource "aws_instance" "demo-server"{
